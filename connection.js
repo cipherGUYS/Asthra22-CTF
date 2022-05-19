5faa9e8b7c3fcf47503e1dd38f1e0b29dd17eefd
reenphygeorge <reenphygeorge@gmail.com>
2022-05-19 21:31:13 +0530

    make it better

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/ctf');
+// mongoose.connect('mongodb://localhost:27017/ctf');
+mongoose.connect('mongodb+srv://cipherGUYS:ALlkuhC1lOzV7WX9@cluster0.kmqap.mongodb.net/?retryWrites=true&w=majority');
+// mongodb+srv://cipherGUYS:<password>@cluster0.kmqap.mongodb.net/?retryWrites=true&w=majority
 var db=mongoose.connection;
 db.on('error', console.log.bind(console, "connection error"));
 db.once('open', function(callback){
@@ -8,3 +9,5 @@ db.once('open', function(callback){
 })
 
 module.exports = db;
+
+// ALlkuhC1lOzV7WX9
